8032317345cf67047134b3d85fc34d68f6b4deb7
adorsk <adorsk@fas.harvard.edu>
2016-12-08 13:27:12 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rdkit_grafting_reaction',
+    version='0.1',
+    description='Grafting reactions using RDKit',
+    author='adorsk',
+    license='MIT',
+    packages=['rdkit_grafting_reaction'],
+    install_requires=['rdkit'],
+    zip_safe=False
+)
